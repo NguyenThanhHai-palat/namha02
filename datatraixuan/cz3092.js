b6336f96047c5ff2cc9056c7e234ddd6b420f609
NguyenThanhHai-palat <nguyenthanhhai12072008@gmail.com>
2026-01-17 23:54:13 +0700

    TEST QR

--- datatraixuan/cz3092.js
+++ datatraixuan/cz3092.js
@@ -1,11 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
-    var url = new URL(window.location.href);
-    var params = url.searchParams;
-    var id = "Quét QR trên vé "
-    if(params.get("id") > 0){
-        id = params.get("id");
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+
+    let id = "Quét QR trên vé";
+    let dataqr = "";
+
+    const paramId = params.get("id");
+    if (paramId && paramId.trim() !== "") {
+        id = paramId;
+        dataqr = "&addInfo=thanh toan ve " + id;
     }
 
-    document.getElementById("id-show").innerHTML = id
+    const idShow = document.getElementById("id-show");
+    const ndckShow = document.getElementById("ndck");
+    if (idShow) {
+        idShow.innerHTML = id;
+        ndckShow.innerHTML = "thanh toan ve "+id
+    }
 
+    const qrCodeImage = document.getElementById("qr-vietqr");
+    if (qrCodeImage) {
+        qrCodeImage.src =
+            `https://img.vietqr.io/image/MB-0917962972-qr_only.png?size=200x200${dataqr}`;
+    } else {
+        console.warn("Không tìm thấy element #qr-vietqr");
+    }
 });
